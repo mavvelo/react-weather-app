24f9095bd38282a9ec053ae2b671d3ed85c172d3
Isaac Cobby Andoh <cobbjy@gmail.com>
2024-10-19 14:35:11 -0400

    updates

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className='App'>
+        <h1>Happy Coding!</h1>
+        <h2>ReactJs Weather App</h2>
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',  // Injects styles into the DOM
+          'css-loader',    // Turns CSS into CommonJS
+          'sass-loader',   // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
